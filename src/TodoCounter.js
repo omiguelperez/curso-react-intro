3b957dbe9f85e6c3429718d6e4101aa88ab7c2f5
Oscar Pérez <mr.omiguelperez@gmail.com>
2024-04-28 21:37:49 -0500

    add span in todocounter

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -3,7 +3,7 @@ import './TodoCounter.css';
 function TodoCounter({ total, completed }) {
   return (
     <h1 className="TodoCounter">
-      You have completed {completed} of {total} TODOs
+      You have completed <span>{completed}</span> of <span>{total}</span> TODOs
     </h1>
   );
 }
