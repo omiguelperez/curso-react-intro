599ec5c90fab9f5975dfbf7a1454e1186d7d4a8b
Oscar Pérez <mr.omiguelperez@gmail.com>
2024-04-28 21:53:47 -0500

    using state

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,12 +1,18 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');
+
+  console.log(`Users are searching for: ${searchValue}`);
+
   return (
     <input
       className="TodoSearch"
       placeholder="Launch rocket to the moon..."
+      value={searchValue}
       onChange={(event) => {
-        console.log('You wrote:', event.target.value);
+        setSearchValue(event.target.value);
       }}
     />
   );
