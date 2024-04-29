6046cdd7ea48e3184c4050668b17e65cb9087016
Oscar Pérez <mr.omiguelperez@gmail.com>
2024-04-29 00:43:56 -0500

    refactor app component to stateless and stateful components

--- src/App/index.js
+++ src/App/index.js
@@ -1,21 +1,6 @@
 import React from 'react';
-import { TodoCounter } from '../TodoCounter';
-import { TodoSearch } from '../TodoSearch';
-import { TodoList } from '../TodoList';
-import { TodoItem } from '../TodoItem';
-import { CreateTodoButton } from '../CreateTodoButton';
 import { useLocalStorage } from './useLocalStorage';
-
-// const defaultTodos = [
-//   { text: 'Make food', completed: true },
-//   { text: 'Take the React.js course', completed: false },
-//   { text: 'Build and deploy a sample project', completed: true },
-//   { text: 'Take the Node.js course', completed: true },
-//   { text: 'Take the NLP course', completed: false },
-//   { text: 'Take the Python course', completed: true },
-// ];
-// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
-// localStorage.removeItem('TODOS_V1');
+import { AppUI } from './AppUI';
 
 function App() {
   const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
@@ -45,24 +30,15 @@ function App() {
   };
 
   return (
-    <>
-      <TodoCounter completed={completedTodos} total={totalTodos} />
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-
-      <TodoList>
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-    </>
+    <AppUI
+      completedTodos={completedTodos}
+      totalTodos={totalTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
