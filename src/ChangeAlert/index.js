9a2c0eee3ff6b3c192073a222f36c456072ff981
Oscar Pérez <mr.omiguelperez@gmail.com>
2024-05-11 14:53:47 -0500

    styles for change alert component

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,11 +1,20 @@
 import { withStorageListener } from './withStorageListener';
+import './ChangeAlert.css';
 
 const ChangeAlert = ({ show, toggleShow }) => {
   if (show) {
     return (
-      <div>
-        <p>There were changes</p>
-        <button onClick={toggleShow}>Refresh</button>
+      <div className="ChangeAlert-bg">
+        <div className="ChangeAlert-container">
+          <p>It seems there were changes in another tab or browser window.</p>
+          <p>Do you want to sync?</p>
+          <button
+            className="TodoForm-button TodoForm-button--add"
+            onClick={toggleShow}
+          >
+            Refresh
+          </button>
+        </div>
       </div>
     );
   }
